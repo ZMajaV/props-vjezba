3c87e1e553586b1dc61ef9e39f8d944df8ac9189
Your Name <you@example.com>
2023-02-07 19:28:47 +0100

    new changes for first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 
-const App = () => {
-  return (
-    <div></div>
-  );
+class App extends Components {
+  users = [
+    {
+    ime: 'Branko',
+    prezime: 'Branković',
+    dob: 32,
+    },
+    {
+    ime: 'Janko',
+    prezime: 'Janković',
+    dob: 42,
+    },
+    {
+    ime: 'Stanko',
+    prezime: 'Stanković',
+    dob: 52,
+    },
+    ];
+  render(){
+    const users = this.users;
+
+    return (
+      <>
+      <CalssComp user= {users[1]} />
+      <FunComp1 user= {users[1]} />
+      <FunComp2>
+        {users}
+      </FunComp2>  
+      </>
+
+    )
+  }
 }
 
 export default App;
